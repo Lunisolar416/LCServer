214e947c27141594888eed8592b7c97dc1deeb18
LUNISOLAR <1739930489@qq.com>
2025-06-30 14:12:02 +0800

    changeto nloman/json

--- GateServer/LogicSystem.cc
+++ GateServer/LogicSystem.cc
@@ -4,48 +4,45 @@
 #include "HttpConnection.h"
 LogicSystem::LogicSystem()
 {
-    RegGet("/get_test",
-           [](std::shared_ptr<HttpConnection> connection)
-           {
-               beast::ostream(connection->_response.body()) << "receive get_test req " << std::endl;
-               int i = 0;
-               for (auto& elem : connection->_get_params)
-               {
-                   i++;
-                   beast::ostream(connection->_response.body())
-                       << "param" << i << " key is " << elem.first;
-                   beast::ostream(connection->_response.body())
-                       << ", " << " value is " << elem.second << std::endl;
-               }
-           });
+    RegGet("/get_test", [](std::shared_ptr<HttpConnection> connection) {
+        beast::ostream(connection->_response.body()) << "receive get_test req " << std::endl;
+        int i = 0;
+        for (auto& elem : connection->_get_params)
+        {
+            i++;
+            beast::ostream(connection->_response.body())
+                << "param" << i << " key is " << elem.first;
+            beast::ostream(connection->_response.body())
+                << ", "
+                << " value is " << elem.second << std::endl;
+        }
+    });
 
-    RegPost("/get_varifycode",
-            [](std::shared_ptr<HttpConnection> connection)
-            {
-                auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
-                std::cout << "receive body is " << body_str << std::endl;
-                connection->_response.set(http::field::content_type, "text/json");
-                Json::Value root;
-                Json::Reader reader;
-                Json::Value src_root;
-                bool parse_success = reader.parse(body_str, src_root);
-                if (!parse_success)
-                {
-                    std::cout << "Failed to parse JSON data!" << std::endl;
-                    root["error"] = ErrorCodes::Error_Json;
-                    std::string jsonstr = root.toStyledString();
-                    beast::ostream(connection->_response.body()) << jsonstr;
-                    return true;
-                }
+    RegPost("/get_varifycode", [](std::shared_ptr<HttpConnection> connection) {
+        auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
+        std::cout << "receive body is " << body_str << std::endl;
+        connection->_response.set(http::field::content_type, "text/json");
 
-                auto email = src_root["email"].asString();
-                std::cout << "email is " << email << std::endl;
-                root["error"] = 0;
-                root["email"] = src_root[std::string("email")];
-                std::string jsonstr = root.toStyledString();
-                beast::ostream(connection->_response.body()) << jsonstr;
-                return true;
-            });
+        json root;
+        try
+        {
+            json src_root = json::parse(body_str);
+
+            auto email = src_root.value("email", "");  // 安全提取
+            std::cout << "email is " << email << std::endl;
+
+            root["error"] = 0;
+            root["email"] = email;
+        }
+        catch (const json::parse_error& e)
+        {
+            std::cout << "Failed to parse JSON data: " << e.what() << std::endl;
+            root["error"] = ErrorCodes::Error_Json;
+        }
+        std::string jsonstr = root.dump(4);  // 格式化输出
+        beast::ostream(connection->_response.body()) << jsonstr;
+        return true;
+    });
 }
 LogicSystem::~LogicSystem()
 {
